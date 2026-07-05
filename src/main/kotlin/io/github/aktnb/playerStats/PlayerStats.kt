9db661cb8cfafba3f5722db1f7ab1ca2e5418eb2
aktnb <70847394+aktnb@users.noreply.github.com>
2026-07-05 09:39:50 +0900

    feat: initial commit

---
+++ src/main/kotlin/io/github/aktnb/playerStats/PlayerStats.kt
@@ -0,0 +1,14 @@
+package io.github.aktnb.playerStats
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PlayerStats : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
